6d01cedade444240b7df9776f80d2b90768419bb
mnus2019 <tontoas2018@gmail.com>
2020-09-08 23:49:10 -0800

    Express Router

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const campsiteRouter = require('./routes/campsiteRouter');
 
 
 const hostname = 'localhost';
@@ -9,6 +10,8 @@ const port = 3000;
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+
+app.use('/campsites', campsiteRouter);
 
 app.all('/campsites', (req, res, next) => {
     res.statusCode = 200;
